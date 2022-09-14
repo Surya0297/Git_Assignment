de820b9b65ceee7c6422630c514f123fba05a9be
surya chauhan <chauhansurya3022gmail.com>
2022-09-14 14:54:12 +0530

    Prime Numbers Check

---
+++ prime_numbers.js
@@ -0,0 +1,12 @@
+let num=13;
+let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(cout>2){
+        console.log("No");
+    }else{
+        conole.log("Yes");
+    }
